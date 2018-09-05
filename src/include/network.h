795f780f35c168936a18160d282bfe62299f3586
Younghan Kim <yhkim2778@gmail.com>
2018-09-06 01:32:17 +0900

    network.cc and network.h are made

---
+++ src/include/network.h
@@ -0,0 +1,37 @@
+#ifndef __network_h_
+#define __network_h_
+
+#include "tensorflow/cc/ops/const_op.h"
+#include "tensorflow/cc/ops/image_ops.h"
+#include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/graph/default_device.h"
+#include "tensorflow/core/graph/graph_def_builder.h"
+#include "tensorflow/core/lib/core/errors.h"
+#include "tensorflow/core/lib/core/stringpiece.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "tensorflow/core/lib/io/path.h"
+#include "tensorflow/core/lib/strings/str_util.h"
+#include "tensorflow/core/lib/strings/stringprintf.h"
+#include "tensorflow/core/platform/env.h"
+#include "tensorflow/core/platform/init_main.h"
+#include "tensorflow/core/platform/logging.h"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/util/command_line_flags.h"
+
+#define OUTPUT_HEIGHT 224
+#define OUTPUT_WIDTH 224
+
+extern float score;//스코어를 저장하는 전역변수
+
+void initNetwork();
+void getSegmentation();
+void caculateScore();
+#endif
+
+
+
+
+
---
+++ src/network.cc
@@ -0,0 +1,19 @@
+#include "include/network.h"
+float score;
+float seg_result[OUTPUT_WIDTH][OUTPUT_HEIGHT]; //세그멘테이션 결과를 저장하는 전역변수 224*224
+
+//네트워크 초기화
+void initNetwork()
+{
+    score = 0;
+}
+//세그멘테이션 결과를 결과배열에 저장한다.
+void getSegmentation()
+{
+    
+}
+//score를 계산하여 전역변수 score에 저장한다.
+void caculateScore()
+{
+    
+}
